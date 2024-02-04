40a3806c697b61c3e3a56208cd1bb8a3e7d83c2d
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-02-04 15:23:46 +0530

    feat: add new commands

--- plugin/tdo.lua
+++ plugin/tdo.lua
@@ -21,3 +21,15 @@ end, {})
 vim.api.nvim_create_user_command('TdoTomorrow', function()
     tdo.run('1')
 end, {})
+
+vim.api.nvim_create_user_command('TdoSearch', function()
+    tdo.search()
+end, {})
+
+vim.api.nvim_create_user_command('TdoFiles', function()
+    tdo.files()
+end, {})
+
+vim.api.nvim_create_user_command('TdoPending', function()
+    tdo.pending()
+end, {})
