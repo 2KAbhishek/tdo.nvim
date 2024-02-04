e9be86333a974f12e4d6aeb12c1d5cb03949b8f9
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-02-04 15:24:04 +0530

    refactor: cleaner api usage

--- lua/tdo/init.lua
+++ lua/tdo/init.lua
@@ -2,7 +2,7 @@ local tdo = {}
 
 tdo.run = function(command)
     local full_command = 'tdo ' .. command
-    local file_name = vim.api.nvim_call_function('system', { full_command })
+    local file_name = vim.fn.system(full_command)
     vim.api.nvim_command('edit ' .. file_name)
 end
 
