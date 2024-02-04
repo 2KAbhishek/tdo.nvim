c5cbe05d0b2119471d28cdb1dd766ded986078d3
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-02-04 14:06:01 +0530

    refactor: add run function

--- lua/tdo/init.lua
+++ lua/tdo/init.lua
@@ -1,7 +1,7 @@
 local tdo = {}
 
-tdo.open_file = function(command)
-    local full_command = command or "tdo"
+tdo.run = function(command)
+    local full_command = 'tdo ' .. command
     local file_name = vim.api.nvim_call_function('system', { full_command })
     vim.api.nvim_command('edit ' .. file_name)
 end
