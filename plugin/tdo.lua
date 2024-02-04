ae864f4520bc7947e2660fa07d5e819469a9d94a
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-02-04 14:06:09 +0530

    feat: add more commands

--- plugin/tdo.lua
+++ plugin/tdo.lua
@@ -7,6 +7,17 @@ vim.g.loaded_tdo = true
 local tdo = require("tdo")
 
 vim.api.nvim_create_user_command('Tdo', function(input)
-    tdo.open_file('tdo' .. ' ' .. input.args)
+    tdo.run(input.args)
 end, { nargs = '*' })
 
+vim.api.nvim_create_user_command('TdoEntry', function(input)
+    tdo.run('entry ' .. input.args)
+end, { nargs = '*' })
+
+vim.api.nvim_create_user_command('TdoYesterday', function()
+    tdo.run('-1')
+end, {})
+
+vim.api.nvim_create_user_command('TdoTomorrow', function()
+    tdo.run('1')
+end, {})
